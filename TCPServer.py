a8c7ca66b07d095e7644da32984ca241ea5704f3
recio862 <recio862@utexas.edu>
2015-05-29 15:50:44 -0500

    initial setup

--- TCPServer.py
+++ TCPServer.py
@@ -1,1 +1,23 @@
 __author__ = 'rjr862'
+import socket, time
+import EventHandler
+
+class TCPServer(EventHandler):
+    def __init__(self, address, client_handler, handler_list):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
+        self.sock.bind(address)
+        self.sock.listen(1)
+        self.client_handler = client_handler
+        self.handler_list = handler_list
+
+    def fileno(self):
+        return self.sock.fileno()
+
+    def receive_ready(self):
+        return True
+
+    def handle_receive(self):
+        client, addr = self.sock.accept()
+        self.handler_list.append(self.client_handler(client, self.handler_list))
+
